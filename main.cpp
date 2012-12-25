3abb0b244214b3711eecfd38459f9dde7c778092
Adrian <adrian.boehm@itonics.de>
2012-12-25 22:35:49 +0100

    SDL test

--- main.cpp
+++ main.cpp
@@ -20,6 +20,22 @@ int main(int argc, char** argv)
     g_pLogfile->CreateLogfile("log.html");
     g_pLogfile->Textout("Programm gestartet", true);
     
+	SDL_Surface* screen = NULL;
+
+    //Start SDL.
+    SDL_Init( SDL_INIT_EVERYTHING );
+    screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+   
+    //Draw a rectangle to the screen.
+    SDL_Rect rect = {10,10,10,10};
+    SDL_FillRect(screen, &rect, 0xFF0000);
+    SDL_Flip(screen);
+
+    //Admire your handiwork for 5 seconds.
+    SDL_Delay(50000);
+   
+    //Quit SDL
+    SDL_Quit();
     
     g_pLogfile->Textout("Programm beendet", false);
     g_pLogfile->Del();
