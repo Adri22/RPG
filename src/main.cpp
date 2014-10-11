e5387a94ca6511b877266e08a3ab22e42da550a1
Adrian Böhm <adrian.boehm22@googlemail.com>
2014-10-12 00:55:03 +0200

    ...

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+
+#include <cstdlib>
+#include <iostream>
+#include "Log.hpp"
+#include "SDL.h"
+#include "Framework.hpp"
+#include "Game.hpp"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    g_pLogfile->CreateLogfile("log.html");
+    g_pLogfile->Textout("programm started", true);
+
+    g_pLogfile->Textout("Initialize framework ...", true);
+    if (g_pFramework->Init(800, 600, 16, false) == false) {
+        g_pLogfile->Textout("Framework initialize failed", true);
+        return (0);
+    } else {
+    	g_pLogfile->Textout("Framework initialized", true);
+    }
+
+    g_pGame->Init();
+    g_pGame->Run();
+    g_pGame->Quit();
+    g_pGame->Del();
+
+    g_pFramework->Quit();
+    g_pFramework->Del();
+
+    g_pLogfile->Textout("exit", true);
+    g_pLogfile->Del();
+
+    return 0;
+}
+
